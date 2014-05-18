c43cf64781c604f33e8c5294be88c5ad29716842
Claudio Contin <claudiocontin@mint>
2014-05-19 11:49:36 +1200

    added hashing for credentials

--- lib/jemquarie/importer.rb
+++ lib/jemquarie/importer.rb
@@ -1,3 +1,6 @@
+require 'digest/sha1'
+require 'base64'
+
 module Jemquarie
 
   class Importer
@@ -13,28 +16,32 @@ module Jemquarie
     end
 
     def cash_transaction(date_from=Date.today, date_to=Date.today)
-      response = @client.call(:generate_xml_extract, message: message)
+      response = @client.call(:generate_xml_extract, :message => create_message(date_from, date_to))
     end
 
 private
 
-    def create_message(date_from, date_to)
-      message = {
-        :string  => Jemquarie.api_key,
+    def create_message(date_from, date_to, account_number=nil)
+      {
+        :string  => hash_key(Jemquarie.api_key), # base64 encoded of the sha1 hashed api key
         :string0 => 'Sharesight Pty Ltd', # TODO - fix this
-        :string1 => @username,
-        :string2 => @password,
+        :string1 => hash_key(@username),
+        :string2 => hash_key(@password),
         :string3 => 'your.clients Transactions',
         :string4 => 'V1.3',
-        :string5 => [
+        :strings => [
           {
-            :item0 => nil, # TODO - let the user specify which account
-            :item1 => date_from.strftime("%Y-%m-%d"),
-            :item2 => date_to.strftime("%Y-%m-%d"),
-            :item3 => 'Y'
+            :item0 => account_number,                 # Account Number - TODO - let the user specify which account
+            :item1 => date_from.strftime("%Y-%m-%d"), # START DATE
+            :item2 => date_to.strftime("%Y-%m-%d"),   # TO DATE
+            :item3 => 'Y'                             # Include closed accounts flag
           }
         ]
       }
+    end
+
+    def hash_key(key)
+      Base64.encode64(Digest::SHA1.hexdigest(key))
     end
 
   end

fa2fcd448c56f81c75f761f9652815e0d57dc4b6
Michael Martins Kuhinica <taverneiro@gmail.com>
2016-10-13 17:10:40 +1300

    Align ident

--- lib/jemquarie/parser/expiry.rb
+++ lib/jemquarie/parser/expiry.rb
@@ -2,7 +2,7 @@ module Jemquarie
   module Parser
     module Expiry
 
-       def parse_expiry_response(response)
+      def parse_expiry_response(response)
         result = generic_request_response(response)
         return result if result[:error]
         result["XMLExtract"]["ExpiryDate"]
